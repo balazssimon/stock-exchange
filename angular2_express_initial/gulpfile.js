6a893aefd7679fdcc0a4846bb9103c0c6ffb0606
Balazs Simon <balazssimon@outlook.com>
2016-10-15 00:00:20 +0200

    Dependencies in gulpfile corrected.

--- angular2_express_initial/gulpfile.js
+++ angular2_express_initial/gulpfile.js
@@ -4,10 +4,10 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var nodemon = require('gulp-nodemon');
 
-gulp.task('default', ['nodemon', 'browser-sync'], function () {
+gulp.task('default', ['browser-sync'], function () {
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', ['nodemon'], function() {
 	browserSync.init(null, {
 		proxy: "http://localhost:3000",
         files: ["app/**/*.*"],
